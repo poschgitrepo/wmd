0405b6e36d78ea9643b9d2645d83efac4703c6bc
Alexander Schultz <61151310+poschgitrepo@users.noreply.github.com>
2020-02-19 18:44:37 +0100

    Add files via upload

---
+++ hiddencode.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 15:46:06 2020
+
+@author: posch
+"""
+
+
+import random as rd
+from numpy import sqrt,zeros
+from matplotlib.pyplot import plot,close
+
+close
+def huge_plot_2(E=70000,SIG_Y=300 ,v=0.3,eta_min=-0.005,eta_max=0.005,NC=500):
+    eta_min=float(eta_min)
+    eta_max=float(eta_max)
+    G=float(E)/(2.*(1+v))
+    SIG_R_1=zeros(NC)
+    SIG_G_1=zeros(NC)
+    SIG_R_2=zeros(NC)
+    SIG_G_2=zeros(NC)
+    for i in range(1,NC):
+       EPS_XX=rd.uniform(eta_min,eta_max)
+       EPS_YY=rd.uniform(eta_min,eta_max)
+       GAM_XY=rd.uniform(eta_min,eta_max)
+       SIG_XX=(E/(1-v**2))*(EPS_XX+v*EPS_YY)
+       SIG_YY=(E/(1-v**2))*(EPS_YY+v*EPS_XX)
+       TAU_XY=G*GAM_XY    
+       SIG_1=((SIG_XX+SIG_YY)/2)+sqrt(((SIG_XX-SIG_YY)/2)**2+TAU_XY**2)
+       SIG_2=((SIG_XX+SIG_YY)/2)-sqrt(((SIG_XX-SIG_YY)/2)**2+TAU_XY**2)
+       SIG_v3=sqrt(0.5*((SIG_1-SIG_2)**2+SIG_1**2+SIG_2**2))
+       f=SIG_v3-SIG_Y   
+       if f<0:
+           SIG_G_1[i]=SIG_1
+           SIG_G_2[i]=SIG_2
+       elif f>0: 
+           SIG_R_1[i]=SIG_1
+           SIG_R_2[i]=SIG_2       
+    plot(SIG_G_1,SIG_G_2,'go')
+    plot(SIG_R_1,SIG_R_2,'ro')  
+    plot(-SIG_G_1,-SIG_G_2,'go')
+    plot(-SIG_R_1,-SIG_R_2,'ro')     
